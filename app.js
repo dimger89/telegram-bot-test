8e382d1c3254d21c88f9bcfe9ebd26df229f171d
Dmytro Herasymenko <dimger89@gmail.com>
2017-10-27 18:54:33 +0300

    bot token

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ var token = '438623317:AAEWmtnG4s2Y5BPYmi12gqbzVpOZqbUP4Gw';
 var nameOfBot = 'AngryMotherFuckerBot';
 var bot = new TelegramBot(token, {polling: true});
 
+var messages = ['Вася', 'Братуха', 'шоты?', 'я AngryMotherFuckerBot'];
+var count = 0;
+
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
+
     console.log(msg);
-    bot.sendMessage(chatId, "Hello!", {caption: `I'm a ${token} bot!`});
+    bot.sendMessage((chatId, messages[count%(messages.length)], {caption: `I'm a ${token} bot!`});
+    count++;
 });
